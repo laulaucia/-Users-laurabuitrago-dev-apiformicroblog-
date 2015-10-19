55c709daac94af773afa3872a8c3689bf18c9bde
Laulaucia <buitrago.laura@gmail.com>
2015-10-18 19:04:42 -0700

    added some post things

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var mongoose = require('mongoose');
 //configuration
 app.set('view engine', 'ejs'); 
 
+mongoose.connect('mongodb://localhost/test');
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
 app.get('/', function (request, response) {
   response.render('index', { title: 'The index page!' });
 });
@@ -19,5 +25,38 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s', host, port);
 });
+ var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+	console.log("CONNECTED TO MONGO");
+	console.log('');
+	console.log('All Systems Go');
+});
 
-app.use(express.static('public'));
+var Schema  = mongoose.Schema;
+
+var PostSchema = new Schema({
+	text: String,
+	
+});
+
+var Posts = mongoose.model('Post', PostSchema);
+
+app.post('/api/post', function(req, res){
+	console.log(req.body);
+	var newPost = Post.create(req.body, function(error, newPost){
+		console.log(newPost);
+	});
+	res.json(newPost);
+});
+
+app.get('/api/post', function(req,res) {
+	Posts.find(function(err, Posts) {
+		res.send(Posts);
+	});
+});
+
+
+
+
+
