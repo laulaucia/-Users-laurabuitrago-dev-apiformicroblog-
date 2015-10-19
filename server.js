cbc9a888b8c6959491af00cdafaa60a9e4329a3a
Laulaucia <buitrago.laura@gmail.com>
2015-10-18 18:40:11 -0700

    added body parser and mongoose

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 
+//required
 var express = require('express');
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-app.set('view engine', 'ejs');  
+//configuration
+app.set('view engine', 'ejs'); 
 
 app.get('/', function (request, response) {
   response.render('index', { title: 'The index page!' });
@@ -12,7 +17,7 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s', host, port);
 });
 
 app.use(express.static('public'));
